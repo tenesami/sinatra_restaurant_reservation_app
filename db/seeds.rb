9831a3ca8fafd6536635573f3ce8f2df5a6c6d8a
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-08-06 16:33:37 -0600

    seeds file to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,10 @@ tesfaye = User.create(user_name: "Tesfaye", email: "tesfa@gmail.com", password: 
 alem = User.create(user_name: "Alem", email: "alem@gmail.com", password: "test")
 
 #make restaurant reservation
-RESERVATIONENTERY.create(restaurant_name: "Nile Etho Restaurant", description: "We will be there at 7.PM and two person sits", user_id: tesfaye.id)
+ReservationEntry.create(restaurant_name: "Nile Etho Restaurant", user_id: tesfaye.id)
 
-tesfaye.reservation_enteries(restaurant_name: "Africana Restaurant", description: "At 1.00PM  and 4 persons sits")
+tesfaye.reservation_entries.create(restaurant_name: "Africana Restaurant")
 
-alem_entry =  alem.reservation_enteries.build(restaurant_name: "Etho peace", description: "2:00pm for 3 persons sit")
+alem_entry =  alem.reservation_entries.build(restaurant_name: "Etho peace")
 alem_entry.save
 
