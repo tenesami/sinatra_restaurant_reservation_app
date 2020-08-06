e7053e0479fed774a9afea9811cf40153013d4bc
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-08-06 16:47:39 -0600

    add user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+    
+    #render the login page (form)
+    get '/login' do
+        
+        erb :login
+    end
+
+    get '/login' do
+  
+    end  
+end
