f04dbf01ec6d60cd0624a386c02aa5f8186eb2ba
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-08-06 21:42:24 -0600

    add user log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,31 @@ class UsersController < ApplicationController
     get '/login' do
         
         erb :login
+        
     end
 
-    get '/login' do
-  
+    #receive the login form, find the user, and log 
+    #the user in (create a session)
+    post '/login' do
+        @user = User.find_by(email: params[:email])
+
+        #Authenticate the user 
+        if @user.authenticate(params[:password])
+            #create a sessions 
+           sessions[:user_id] = @user.id #actually log user in
+           puts session
+           redirect "user/#{user.id}"
+        else
+
+        end
+
     end  
+
+    get '/signup' do
+    end
+
+    get '/user/:id' do 
+       "this will be the user show route " 
+    end
+
 end
