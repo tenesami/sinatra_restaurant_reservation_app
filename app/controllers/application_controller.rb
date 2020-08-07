5c361ecd7ec614a01e423286b783529ed0f29a63
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-08-06 21:40:06 -0600

    add sesssion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-  end
+  #   enable :sessions
+  #   set :session_secret, "restauran_reservation_app"
+   end
 
   get "/" do
     erb :welcome
