c1387a15eb3cbcc131b4be83ae3a34d3c61c0a23
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-08-06 21:44:25 -0600

    add sesssion in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-  #   enable :sessions
-  #   set :session_secret, "restauran_reservation_app"
+    enable :sessions
+    set :session_secret, "restauran_reservation_app"
    end
 
   get "/" do
