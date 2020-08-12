797aa97bd1ded2cf9043bd69d42b9d9a1321bf58
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-08-12 03:14:57 -0600

    add active recorerd validation logic inside user model To validate user input

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base 
     has_secure_password
 
+    validates :user_name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+
     has_many :reservation_entries
 end
