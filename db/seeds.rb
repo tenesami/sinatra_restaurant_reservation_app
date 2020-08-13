25101eff0c6d7ad6f5c3de729ff37ffc6747f28d
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-08-13 01:55:32 -0600

    seed new data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,12 @@
 tesfaye = User.create(user_name: "Tesfaye", email: "tesfa@gmail.com", password: "test")
 alem = User.create(user_name: "Alem", email: "alem@gmail.com", password: "test")
 
-#make restaurant reservation
-ReservationEntry.create(restaurant_name: "Nile Etho Restaurant", user_id: tesfaye.id)
+#makes restaurant reservation 
+ReservationEntry.create(restaurant_name: "chili's ", numOfTable: "2", user_id: tesfaye.id)
 
-tesfaye.reservation_entries.create(restaurant_name: "Africana Restaurant")
+# makes restaurant reservation using AR to pre-associate data:
+tesfaye.reservation_entries.create(restaurant_name: "Bamboo sushi", numOfTable: "2")
 
-alem_entry =  alem.reservation_entries.build(restaurant_name: "Etho peace")
+alem_entry =  alem.reservation_entries.build(restaurant_name: "Nile", numOfTable: "4")
 alem_entry.save
 
