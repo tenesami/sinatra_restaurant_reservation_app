770ae7bec4d230a1746bae88e45150c62c07882e
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-08-13 21:02:00 -0600

    add comment to exlplain the code and add flash message

--- app/controllers/reservation_entries_controller.rb
+++ app/controllers/reservation_entries_controller.rb
@@ -1,41 +1,51 @@
 class ReservationEntriesController < ApplicationController
     
-
+#show  out all the reservation entries
     get '/reservation_entries' do
         @reservation_entries = ReservationEntry.all
         erb :'/reservation_entries/index'
       end
 
+    #read the new form reservation entry
+    # get reservation_entries/new to render a form to create new entry
     get '/reservation_entries/new' do
+        redirect_if_not_logged_in
         erb :'/reservation_entries/new'
     end
    
+    #create or post reservation_entries to create a new journal entry
     post '/reservation_entries' do
-        #redirectto the home page _if_not_logged_in
-        redirect_if_logged_in
+        #redirect to the home page _if_not_logged_in
+        redirect_if_not_logged_in
         
         if params[:restaurant_name] != ""
-
+            # create a new entry
             @reservation_entry = ReservationEntry.create(
                 restaurant_name: params[:restaurant_name], 
+                numOfTable: params[:numOfTable],
                 user_id: current_user.id)
-            flash[:message] = "Successfully Resreve Tabel." if @reservation_entry.id
+                # notiafay users if they created reservation succesfully 
+                #if reservation.id is created 
+            flash[:message] = "You Successfully Reserev restaurant tabel." if @reservation_entry.id
             redirect "/reservation_entries/#{@reservation_entry.id}"
         else
+            flash[:errors] = "you must provide number of table for your entry."
             redirect '/reservation_entries/new'
         end
     end
 
+    # show route for a journal entry
     get '/reservation_entries/:id' do
         set_reservation_entry
         erb :'/reservation_entries/show'
     end
 
-
+    # Send us to reservation_entries/edit.erb, which will
+    # render an edit form
     get '/reservation_entries/:id/edit' do
-        set_reservation_entry
+
         redirect_if_not_logged_in
-    
+        set_reservation_entry
         if authorized_to_edit?(@reservation_entry)
             erb :'/reservation_entries/edit'
         else
@@ -43,12 +53,13 @@ class ReservationEntriesController < ApplicationController
         end
     end
 
-    
+    #find the reservation entry, update the reservation entry, redirect to show page
     patch '/reservation_entries/:id' do
-        set_reservation_entry
         redirect_if_logged_in
-    
-        if authorized_to_edit?(@reservation_entry) && params[:restaurant_name] != ""  
+
+        set_reservation_entry
+        
+        if @reservation_entry.user == current_user && params[:restaurant_name] != ""  
         
         @reservation_entry.update(restaurant_name: params[:restaurant_name])
         
@@ -56,18 +67,21 @@ class ReservationEntriesController < ApplicationController
         else
             redirect "/users/#{current_user.id}"
         end
-    
     end
 
+    #find the reservation entry, delete the reservation entry redirect 
+    #reservation entries
     delete '/reservation_entries/:id' do
         set_reservation_entry
         if authorized_to_edit?(@reservation_entry)
             @reservation_entry.destroy
+            flash[:message] = "Successfully deleted that entry."
             redirect '/reservation_entries'
          else
             redirect '/reservation_entries'
         end
     end
+
     private 
     #helper method
     def set_reservation_entry
