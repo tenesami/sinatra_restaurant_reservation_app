7255135168a9633764ee677610dba708f01ba6a3
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-08-28 10:15:00 -0600

    add the active record association build to relate the reservation

--- app/controllers/reservation_entries_controller.rb
+++ app/controllers/reservation_entries_controller.rb
@@ -1,44 +1,26 @@
 class ReservationEntriesController < ApplicationController
     
-#show  out all the reservation entries
     get '/reservation_entries' do
-        @reservation_entries = ReservationEntry.all
-        #@reservation_entries = current_user.reservation_entries
-        #@reservation_entries = ReservationEntry.where(:user => current_user)
-        
+        @reservation_entries = ReservationEntry.all  
+       
         erb :'/reservation_entries/index'
-      end
+    end
 
-    #read the new form reservation entry
-    # get reservation_entries/new to render a form to create new entry
     get '/reservation_entries/new' do
         redirect_if_not_logged_in
         erb :'/reservation_entries/new'
     end
    
-    #create or post reservation_entries to create a new journal entry
+    
     post '/reservation_entries' do
-        #redirect to the home page _if_not_logged_in
+        
         redirect_if_not_logged_in
-        
-        # @reservation_entry = reservation_entry.new
-        # @reservation_entry.restaurant_name = params[:restaurant_name]
-        # @reservation_entry.user_id = current_user.id
-
-        #if @reservation_entry.save
-        #    redirect '/reservation_entries'
-        #else
-        #    erb :'/reservation_entries/new'
-        
-
+       
         if params[:restaurant_name] != "" && params[:numOfTable] != ""
-            # create a new entry
-            @reservation_entry = ReservationEntry.create(
+            @reservation_entry = current_user.reservation_entries.build(  
                 restaurant_name: params[:restaurant_name], 
-                numOfTable: params[:numOfTable],
-                user_id: current_user.id)
-                # notiafay users if they created reservation succesfully 
-                #if reservation.id is created 
+                numOfTable: params[:numOfTable])
+                
             flash[:message] = "You Successfully Reserev restaurant tabel." if @reservation_entry.id
             redirect "/reservation_entries/#{@reservation_entry.id}"
         else
@@ -47,28 +29,25 @@ class ReservationEntriesController < ApplicationController
         end
     end
 
-    # show route for a journal entry
+    
     get '/reservation_entries/:id' do
         set_reservation_entry
         erb :'/reservation_entries/show'
     end
 
-    # Send us to reservation_entries/edit.erb, which will
-    # render an edit form
+
     get '/reservation_entries/:id/edit' do
-
         redirect_if_not_logged_in
-
         set_reservation_entry
+        
         if authorized_to_edit?(@reservation_entry)
             erb :'/reservation_entries/edit'
-        else
-            
+        else       
             redirect "/users/#{current_user.id}"
         end
     end
 
-    #find the reservation entry, update the reservation entry, redirect to show page
+
     patch '/reservation_entries/:id' do
         redirect_if_not_logged_in
 
@@ -85,8 +64,7 @@ class ReservationEntriesController < ApplicationController
         end
     end
 
-    #find the reservation entry, delete the reservation entry redirect 
-    #reservation entries
+    
     delete '/reservation_entries/:id' do
         set_reservation_entry
         if authorized_to_edit?(@reservation_entry)
@@ -98,9 +76,9 @@ class ReservationEntriesController < ApplicationController
         end
     end
 
+
     private 
-    #helper method
     def set_reservation_entry
-        @reservation_entry = ReservationEntry.find(params[:id])
+        @reservation_entry = ReservationEntry.find_by(params[:id])
     end
 end
